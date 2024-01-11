8eefefde1a35ffc1bd532435bc0b58a7ce1ca410
k21govind <k21govind@gmail.com>
2024-01-12 00:59:30 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-test-1';
+  tabElements:any[]=["Tab Name1","Tab Name2","Tab Name3"]
+}
